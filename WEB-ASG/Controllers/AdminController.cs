587c2216b119ee15118e7dc33e66c55e44bb5951
RusselldeSilva <s10203242@connect.np.edu.sg>
2021-05-27 22:14:08 +0800

    Add Administrator Views, nav bar links on home/index

---
+++ WEB-ASG/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_ASG.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewAreaInterest()
+        {
+            return View();
+        }
+        public IActionResult CreateAreaInterest()
+        {
+            return View();
+        }
+        public IActionResult EditComp()
+        {
+            return View();
+        }
+        public IActionResult AddJudge()
+        {
+            return View();
+        }
+    }
+}
