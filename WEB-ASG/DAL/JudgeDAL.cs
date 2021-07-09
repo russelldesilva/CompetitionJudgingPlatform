492811bdb510ff23b19a9e9357863ff280dd49cc
Mxrcxsz <s10208495@connect.np.edu.sg>
2021-07-09 16:24:14 +0800

    Added GetAllJudges()

--- WEB-ASG/DAL/JudgeDAL.cs
+++ WEB-ASG/DAL/JudgeDAL.cs
@@ -22,6 +22,31 @@ namespace WEB_ASG.DAL
             string strConn = Configuration.GetConnectionString("CJPConnectionString");
             conn = new SqlConnection(strConn);
         }
+        public List<Judge> GetAllJudges()
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM Judge ORDER BY JudgeID";
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Judge> judgeList = new List<Judge>();
+            while (reader.Read())
+            {
+                judgeList.Add(
+                    new Judge
+                    {
+                        JudgeID = reader.GetInt32(0),
+                        JudgeName = reader.GetString(1),
+                        Salutation = reader.GetString(2),
+                        AreaInterestID = reader.GetInt32(3),
+                        EmailAddr = reader.GetString(4),
+                        Password = reader.GetString(5),
+                    }
+                );
+            }
+            reader.Close();
+            conn.Close();
+            return judgeList;
+        }
         public List<Judge> GetJudges(int areaID)
         {
             SqlCommand cmd = conn.CreateCommand();
