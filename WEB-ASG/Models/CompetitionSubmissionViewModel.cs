2abaebd1770ac6b409cd9b208b56a5dfa8ea019b
Mxrcxsz <s10208495@connect.np.edu.sg>
2021-07-11 05:07:03 +0800

    Added CompetitionSubmissionViewModel

--- WEB-ASG/Models/CompetitionSubmissionViewModel.cs
+++ WEB-ASG/Models/CompetitionSubmissionViewModel.cs
@@ -1,28 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace WEB_ASG.Models
 {
-    public class CompetitionSubmission
+    public class CompetitionSubmissionViewModel
     {
         [Display(Name = "Competition ID")]
         public int CompetitionID { get; set; }
+        [Display(Name = "Competition")]
+        public string CompetitionName { get; set; }
         [Display(Name = "Competitor ID")]
         public int CompetitorID { get; set; }
-        [Display(Name = "File name")]
+        [Display(Name = "Upload Work")]
+        public IFormFile fileToUpload { get; set; }
+        [StringLength(255, ErrorMessage = "File can only have a maximum of 255 characters!"), ValidateFileFormat]
         public string FileSubmitted { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}")]
         public DateTime? DateTimeFileUpload { get; set; }
-        [Display(Name = "Appeal")]
-        [StringLength(255, ErrorMessage = "File can only have a maximum of 255 characters!")]
-        public string Appeal { get; set; }
-        [Display(Name = "Vote Count")]
-        public int VoteCount { get; set; }
-        [Display(Name = "Ranking")]
-        public int? Ranking { get; set; }
     }
 }
