dbf18eb5eb335fc6598c5630ef3df57c56f6ecdb
s10205505@connect.np.edu.sg <s10205505@connect.np.edu.sg>
2021-07-11 16:52:51 +0800

    ValidateWeightage Model

---
+++ WEB-ASG/Models/ValidateWeightage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_ASG.DAL;
+
+namespace WEB_ASG.Models
+{
+    public class ValidateWeightage : ValidationAttribute
+    {
+        private JudgeDAL judgeContext = new JudgeDAL();
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            // Casting the validation context to the "Staff" model class
+            Criteria criteria = (Criteria)validationContext.ObjectInstance;
+            int weightage = Convert.ToInt32(value);
+            
+            // Get the Staff Id from the staff instance
+            int competitionID = criteria.CompetitionID;
+            int criteriaID = criteria.CriteriaID;
+            if (judgeContext.IsWeightageRight(competitionID, weightage, criteriaID))
+                // validation failed
+                return new ValidationResult
+                ("Weightage is above 100!");
+            else
+                // validation passed
+                return ValidationResult.Success;
+        }
+    }
+
+}
