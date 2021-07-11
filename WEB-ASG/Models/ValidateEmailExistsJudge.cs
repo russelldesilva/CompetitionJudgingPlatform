0e3519079658a1f6feb558c4730da46bb9058fcf
s10205505@connect.np.edu.sg <s10205505@connect.np.edu.sg>
2021-07-11 16:52:41 +0800

    ValidateEmail Model

---
+++ WEB-ASG/Models/ValidateEmailExistsJudge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using WEB_ASG.DAL;
+
+namespace WEB_ASG.Models
+{
+    public class ValidateEmailExistsJudge : ValidationAttribute
+    {
+        private JudgeDAL judgeContext = new JudgeDAL();
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            // Get the email value to validate
+            string email = Convert.ToString(value);
+            // Casting the validation context to the "Staff" model class
+            Judge judge = (Judge)validationContext.ObjectInstance;
+            // Get the Staff Id from the staff instance
+            int judgeId = judge.JudgeID;
+            if (judgeContext.IsEmailExist(email, judgeId))
+                // validation failed
+                return new ValidationResult
+                ("Email address already exists!");
+            else
+                // validation passed    
+                return ValidationResult.Success;
+        }
+    }
+}
